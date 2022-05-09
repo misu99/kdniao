2f0c8c6279664f8efbdbb2475ff6bb677292ac3e
ade <798449992@qq.com>
2022-05-09 20:11:30 +0800

    返回字段使用驼峰规则

--- response/monitor_subscribe_response.go
+++ response/monitor_subscribe_response.go
@@ -1,12 +1,12 @@
 package response
 
 type MonitorSubscribeResponse struct {
-	EBusinessId  string `json:"EBusinessID"`  // 用户ID
-	UpdateTime   string `json:"UpdateTime"`   // 更新时间，示例： 2021-01-01 09:00:00
-	Success      bool   `json:"Success"`      // 成功与否(true/false)
-	ShipperCode  string `json:"ShipperCode"`  // 快递公司编码
-	LogisticCode string `json:"LogisticCode"` // 快递单号
-	Reason       string `json:"Reason"`
+	EBusinessId  string `json:"eBusinessId"`  // 用户ID
+	UpdateTime   string `json:"updateTime"`   // 更新时间，示例： 2021-01-01 09:00:00
+	Success      bool   `json:"success"`      // 成功与否(true/false)
+	ShipperCode  string `json:"shipperCode"`  // 快递公司编码
+	LogisticCode string `json:"logisticCode"` // 快递单号
+	Reason       string `json:"reason"`
 }
 
 func (resp *MonitorSubscribeResponse) IsSuccess() bool {
